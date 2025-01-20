30a7c137407896613f145dec72488e12c18694ab
Atire <96929298+At1re@users.noreply.github.com>
2025-01-20 00:19:54 -0500

    Update GuessChecker134.java
    Fixed issues that the code was having. The code was incorrect because it was missing else if statements and curly braces.

--- GuessChecker134.java
+++ GuessChecker134.java
@@ -1,34 +1,38 @@
 import java.util.Scanner;
 
+
 public class GuessChecker134
 {
     public static void main(String[] args)
     {
         /* Add any variables you will need throughout the program here. */
 
+
         // Generate the random number
         String targetStr = makeCode();
         System.out.println("Random Number (for debugging): " + targetStr);  // uncomment for debugging
-        
+       
         // Break the random number into four variables.
         int r1 = Integer.parseInt(targetStr.substring(0, 1));
         int r2 = Integer.parseInt(targetStr.substring(1, 2));
         int r3 = Integer.parseInt(targetStr.substring(2, 3));
         int r4 = Integer.parseInt(targetStr.substring(3, 4));
-        
+       
         // Get the user's guess.
         String guess = getGuess();
         // System.out.println(guess);   // uncomment for debugging
-        
+       
         // Break the user's guess into four variables.
         int g1 = Integer.parseInt(guess.substring(0, 1));
         int g2 = Integer.parseInt(guess.substring(1, 2));
         int g3 = Integer.parseInt(guess.substring(2, 3));
         int g4 = Integer.parseInt(guess.substring(3, 4));
 
+
         /*your code here*/
         int hits = 0; // Initialize the hit counter
         int nearHits = 0; // Initialize the near hit counter
+
 
         // Compare each digit of the guess to the corresponding digit of the random number
         if (g1 == r1) {
@@ -39,6 +43,7 @@ public class GuessChecker134
             if (g1 == r4) nearHits++;
         }
 
+
         if (g2 == r2) {
             hits++;
         } else {
@@ -46,6 +51,7 @@ public class GuessChecker134
             if (g2 == r3) nearHits++;
             if (g2 == r4) nearHits++;
         }
+
 
         if (g3 == r3) {
             hits++;
@@ -55,6 +61,7 @@ public class GuessChecker134
             if (g3 == r4) nearHits++;
         }
 
+
         if (g4 == r4) {
             hits++;
         } else {
@@ -62,7 +69,7 @@ public class GuessChecker134
             if (g4 == r2) nearHits++;
             if (g4 == r3) nearHits++;
         }
-        
+       
         // Display the total number of hits and near hits
         System.out.println("Total hits: " + hits);
         System.out.println("Total near hits: " + nearHits);
@@ -70,7 +77,7 @@ public class GuessChecker134
  
     // Checks to ensure no duplicate digits in a number.
     public static boolean hasDupes(int num)
-    { 
+    {
         boolean[] digs = new boolean[10];
         while (num > 0)
         {
@@ -82,9 +89,10 @@ public class GuessChecker134
         return false;
     }
 
+
     // Creates a new random 4 digit code 1000-9999 with no duplicates.
-    public static String makeCode() 
-    { 
+    public static String makeCode()
+    {
         int target = (int) (Math.random() * 9000 + 1000);
         while (hasDupes(target))
           target = (int) (Math.random() * 9000 + 1000);
@@ -92,9 +100,10 @@ public class GuessChecker134
         return targetStr;
     }
 
+
     // Prompts the user for a guess and repeats until valid guess is made.
-    public static String getGuess() 
-    { 
+    public static String getGuess()
+    {
         Scanner sc = new Scanner(System.in);
         boolean validGuess = false;
         int userGuess = 0;
@@ -102,10 +111,12 @@ public class GuessChecker134
         {
             System.out.print("Guess a 4-digit number from 1000 to 9999 with no duplicate digits: ");
             userGuess = sc.nextInt();
-            if (!(hasDupes(userGuess) || (userGuess < 1000))) 
+            if (!(hasDupes(userGuess) || (userGuess < 1000)))
                 validGuess = true;
         }
         String userStr = userGuess + "";
         return userStr;
     }
 }
+
+
